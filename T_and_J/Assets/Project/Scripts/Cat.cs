b2c29619f179d464ab7439deaed45f9e2e3e221e
Ryo-suzusan <ryo.ohta1017@gmail.com>
2025-05-29 11:47:26 +0900

    sasaki no kawari

--- T_and_J/Assets/Project/Scripts/Cat.cs
+++ T_and_J/Assets/Project/Scripts/Cat.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Cat : MonoBehaviour
@@ -7,9 +8,12 @@ public class Cat : MonoBehaviour
 
     [SerializeField]
     GameObject mouse;
-
     [Header("追跡の速さ")]
-    private float chaseSpeed_ = 0.5f;
+    private float chaseSpeed_ = 0.125f;
+
+    int y = 0;
+    int cnt = 0;
+    public List<int> go = new List<int>();
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +24,50 @@ public class Cat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = (mouse.transform.position - this.transform.position).normalized;
-        //指定した量進む
-        float vx = dir.x * chaseSpeed_;
-        float vz = dir.z * chaseSpeed_;
-        this.transform.Translate(vx / 50, 0, vz / 50);
+        float vx = 3 * chaseSpeed_;
+        if (go[y] == 0) this.transform.Translate(vx / 15, 0, 0);
+        if (go[y] == 1) this.transform.Translate(0, 0, vx / 15);
+        if (go[y] == 2) this.transform.Translate(-vx / 15, 0, 0);
+        if (go[y] == 3) this.transform.Translate(0, 0, -vx / 15);
+        cnt++;
+        if (cnt == 5)
+        {
+            y++;
+            cnt = 0;
+        }
+        if (go.Count == y) y = 0;
     }
 
     public void startDFS()
     {
-
+        bool[,] map;
+        List<int> dx = new List<int>() { 1, 0, -1, 0 };
+        List<int> dy = new List<int>() { 0, 1, 0, -1 };
+        bool[,] v = new bool[GameManager.instance.wallMap.GetLength(0), GameManager.instance.wallMap.GetLength(0)];
+        void DFS(int nowx, int nowy, int muki)
+        {
+            bool move = false;
+            for (int i = 0; i < 4; i++)
+            {
+                int ni = nowx + dx[i], nj = nowy + dy[i];
+                if (ni < 0 || ni >= GameManager.instance.wallMap.GetLength(0) || nj < 0 || nj >= GameManager.instance.wallMap.GetLength(0)) continue;
+                if (v[ni, nj]) continue;
+                if (GameManager.instance.wallMap[ni, nj]) continue;
+                v[ni, nj] = true;
+                go.Add(i);
+                DFS(ni, nj, i);
+                move = true;
+            }
+            if (!move) go.Add((muki + 2) % 4);
+        }
+        DFS(1, 1, -1);
+        Debug.Log(GameManager.instance.wallMap.GetLength(0));
+        /*for(int i = 0; i < GameManager.instance.wallMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < GameManager.instance.wallMap.GetLength(0); j++)
+            {
+                Debug.Log(GameManager.instance.wallMap[i, j]);
+            }
+        }*/
     }
 }
