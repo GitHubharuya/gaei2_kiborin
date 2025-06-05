727ab0a218cf9433da0c921ad284da2a63c54bdc
takor1ce <kotagame0718@gmail.com>
2025-06-05 12:16:54 +0900

    MouseCamera and Controller

--- T_and_J/Assets/Project/Scripts/MouseCamera.cs
+++ T_and_J/Assets/Project/Scripts/MouseCamera.cs
@@ -1,19 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCamera : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public Transform target;         // 追いかける対象（ネズミ）
-    public Vector3 offset = new Vector3(0, 5, -7); // 高さと後ろからの距離
-    public float smoothSpeed = 5f;   // カメラの追従速度
+    [SerializeField] GameObject player;
 
-    void LateUpdate()
+    Vector3 currentPos;//現在のカメラ位置
+    Vector3 pastPos;//過去のカメラ位置
+
+    Vector3 diff;//移動距離
+
+    private void Start()
     {
-        if (target == null) return;
+        //最初のプレイヤーの位置の取得
+        pastPos = player.transform.position;
+    }
+    void Update()
+    {
+        //------カメラの移動------
 
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        //プレイヤーの現在地の取得
+        currentPos = player.transform.position;
 
-        transform.position = smoothedPosition;
-        transform.LookAt(target); // ネズミを見る
+        diff = currentPos - pastPos;
+
+        transform.position = Vector3.Lerp(transform.position, transform.position + diff, 1.0f);//カメラをプレイヤーの移動差分だけうごかすよ
+
+        pastPos = currentPos;
+
+
+        //------カメラの回転------
+
+        // マウスの移動量を取得
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+
+        // X方向に一定量移動していれば横回転
+        if (Mathf.Abs(mx) > 0.01f)
+        {
+            // 回転軸はワールド座標のY軸
+            transform.RotateAround(player.transform.position, Vector3.up, mx);
+        }
+
+        // Y方向に一定量移動していれば縦回転
+        if (Mathf.Abs(my) > 0.01f)
+        {
+            // 回転軸はカメラ自身のX軸
+            transform.RotateAround(player.transform.position, transform.right, -my);
+        }
     }
 }
