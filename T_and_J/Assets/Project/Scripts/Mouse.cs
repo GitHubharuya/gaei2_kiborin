1043e66a092311c309c71a1657daabd061bb10a3
Ryo-suzusan <ryo.ohta1017@gmail.com>
2025-06-19 02:08:41 +0900

    collision

--- T_and_J/Assets/Project/Scripts/Mouse.cs
+++ T_and_J/Assets/Project/Scripts/Mouse.cs
@@ -5,40 +5,17 @@ using UnityEngine;
 public class Mouse : MonoBehaviour
 {
     [SerializeField]
-    GameObject cat;
-
-    [Header("逃走の速さ")]
-    private float runSpeed_ = 0.5f;
-
-    public bool isInWall = false;
-
-    // Start is called before the first frame update
-    void Start()
+    GameModeSetting gameModeSetting;
+    public void OnTriggerEnter(Collider other)
     {
-        
+        if (other.name == "Cat")
+        {
+            //GameModeSettingにネズミと衝突した合図を送る
+            gameModeSetting.finishGame();
+            beEaten();
+            Debug.LogError("食べられた！");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 dir = (cat.transform.position - this.transform.position).normalized;
-        //指定した量進む
-        float vx = dir.x * runSpeed_;
-        float vz = dir.z * runSpeed_;
-        this.transform.Translate(-vx / 50, 0, -vz / 50);
-    }
-
-    public void changeBoolTrue()
-    {
-        isInWall = true;
-        Debug.Log("入ったよ");
-    }
-    public void changeBoolFalse()
-    {
-        isInWall = false;
-        Debug.Log("出たよ");
-    }
-
     public void beEaten()
     {
         Destroy(gameObject);
--- T_and_J/Assets/Project/Scripts/PlayerController.cs
+++ T_and_J/Assets/Project/Scripts/PlayerController.cs
@@ -78,8 +78,6 @@ public class PlayerController : MonoBehaviour
             moveSpeed += moveSpeedIn * cameraRight;
         }
 
-        Debug.Log($"W:{Input.GetKey(KeyCode.W)} S:{Input.GetKey(KeyCode.S)} moveSpeed:{moveSpeed}");
-
         Move();
 
         //慣性を消す
@@ -101,7 +99,7 @@ public class PlayerController : MonoBehaviour
         //過去の位置の更新
         pastPos = currentPos;
 
-        if (delta == Vector3.zero)
+        if (delta.magnitude < 0.01f || moveSpeed.magnitude < 1)
             return;
 
         playerRot = Quaternion.LookRotation(delta, Vector3.up);
